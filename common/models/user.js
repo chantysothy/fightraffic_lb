526fe49c4a69f35f51e2ee035239b9231c6ca156
yadavji83 <yadavji83@gmail.com>
2015-12-30 02:17:58 +0530

    Made changes for car edit options

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,19 @@
 module.exports = function(User) {
   
-  	
+  	/*
   
+  User.getUserData = function(tokenId, fn) {
+    this.relations.accessTokens.modelTo.findById(tokenId, function(err, accessToken) {
+      if (err) {
+        fn(err);
+      } else if (accessToken) {
+        this.relations.identities.modelTo.findById(User.id, function(err, identities) {
+			return identities;
+		});
+      } else {
+        fn(new Error('could not find accessToken'));
+      }
+    });
+  };
+  */
 };
