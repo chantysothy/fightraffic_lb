5b9d7621ba52520cc4c90896f153eec905763152
yadavji83 <yadavji83@gmail.com>
2015-12-31 17:40:56 +0530

    removed sample data

--- server/boot/create-table-car.js
+++ server/boot/create-table-car.js
@@ -3,9 +3,9 @@ module.exports = function(app) {
     if (err) throw err;
  
     app.models.Car.create([
-      {name: 'Car1', userId:1},
-      {name: 'Car2', userId:2},
-      {name: 'Car3', oddEven:'odd', image:'img/car-1.jpg', totalSeats:5, seatsLeft:2, isSun:true, startHr:12, startMin:30, startAMPM:'AM', endHr:06, endMin:10, endAMPM:'PM', startPlace:'Haiderpur', endPlace:'Gurgaon', milage:24, distance:30, message:"Smoking and Drinking will be allowed and will be done", userId:4},
+      /*{name: 'Car1', userId:1},
+      {name: 'Car2', userId:2},*/
+      {name: 'Sample Car', oddEven:'odd', image:'img/car-1.jpg', totalSeats:5, seatsLeft:2, isSun:true, startHr:12, startMin:30, startAMPM:'AM', endHr:06, endMin:10, endAMPM:'PM', startPlace:'Haiderpur', endPlace:'Gurgaon', milage:24, distance:30, message:"Smoking and Drinking will not be allowed", userId:4},
     ], function(err, Car) {
       if (err) throw err;
  
@@ -17,9 +17,9 @@ module.exports = function(app) {
     if (err) throw err;
  
     app.models.user.create([
-      {name: 'User1', password: 'sdsdsd', email: 'demo@demo.com',carId:1},
-      {name: 'User2', password: 'sdsdsd', email: 'demo2@demo.com',carId:2},
-      {name: 'User3', password: 'sdsdsd', email: 'demo3@demo.com',carId:32},
+      /*{name: 'User1', password: 'sdsdsd', email: 'demo@demo.com',carId:1},
+      {name: 'User2', password: 'sdsdsd', email: 'demo2@demo.com',carId:2},*/
+      {name: 'Sample User', password: 'sdsdsd', email: 'demo3@demo.com',carId:32},
     ], function(err, user) {
       if (err) throw err;
  
